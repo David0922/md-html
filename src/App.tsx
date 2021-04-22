7ff591c1c3d04b180867c82a6ab1a0a972db6497
David Chen <davidchen94@outlook.com>
2021-04-22 03:15:52 -0700

    _

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import hljs from 'highlight.js';
+import 'highlight.js/styles/stackoverflow-dark.css';
+import React, { useCallback, useEffect, useRef } from 'react';
+import { Remarkable } from 'remarkable';
 import './App.css';
+import useWindowDimensions from './useWindowDimensions';
 
 function App() {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const rawRef = useRef<HTMLTextAreaElement>(null);
+  const htmlRef = useRef<HTMLDivElement>(null);
+
+  const mdRef = useRef(
+    // https://github.com/jonschlinkert/remarkable#syntax-highlighting
+    new Remarkable({
+      highlight: (str, lang) => {
+        if (lang && hljs.getLanguage(lang)) {
+          try {
+            return hljs.highlight(lang, str).value;
+          } catch (err) {}
+        }
+
+        try {
+          return hljs.highlightAuto(str).value;
+        } catch (err) {}
+
+        return ''; // use external default escaping
+      }
+    })
+  );
+
+  const { height: windowHeight } = useWindowDimensions();
+
+  const modifyCode = useCallback(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      if (htmlRef.current) {
+        htmlRef.current.innerHTML = mdRef.current.render(e.target.value);
+
+        htmlRef.current
+          .querySelectorAll('pre code')
+          .forEach(block => hljs.highlightBlock(block as HTMLElement));
+      }
+    },
+    []
+  );
+
+  useEffect(() => {
+    if (rawRef.current) rawRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    if (wrapperRef.current)
+      wrapperRef.current.style.height = `${windowHeight}px`;
+  }, [windowHeight]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={wrapperRef} className='wrapper'>
+      <textarea ref={rawRef} onChange={modifyCode} className='raw'></textarea>
+      <div ref={htmlRef} className='rendered'></div>
     </div>
   );
 }
